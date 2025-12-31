5c732e63ab2594134c3436f7579eb6f8c9161066
MikeWKI <Michaela@WichitaKenworth.com>
2025-12-31 10:12:53 -0600

    Fix console warnings: remove missing favicon refs, update deprecated meta tag, add autocomplete to password

--- src/RepairOrderTracker.tsx
+++ src/RepairOrderTracker.tsx
@@ -1057,6 +1057,7 @@ const RepairOrderTracker = () => {
                   }}
                   className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 bg-white"
                   placeholder="Enter password"
+                  autoComplete="current-password"
                   autoFocus
                 />
                 {passwordError && (
